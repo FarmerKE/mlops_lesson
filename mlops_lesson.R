2222e7ba6a1496acecb1b25b39b4d1aa36be8d77
FarmerKE <kiokokm79@gmail.com>
2024-02-18 22:13:29 +0300

    Build model and fit final model.

--- mlops_lesson.R
+++ mlops_lesson.R
@@ -8,6 +8,7 @@ library(pins)
 library(vetiver)
 library(plumber)
 library(conflicted)
+library(torch)
 tidymodels_prefer()
 options(tidymodels.dark = TRUE)
 conflicts_prefer("penguins", "palmerpenguins")
@@ -28,8 +29,103 @@ penguins_df <- palmerpenguins::penguins %>%
   drop_na(sex) %>% 
   select(-year, -island)
 
+# Training and Test split
 set.seed(1234)
 
 penguin_split <- initial_split(penguins_df, strata = sex)
 penguin_train <- training(penguin_split)
 penguin_test <- testing(penguin_split)
+
+penguin_folds <- vfold_cv(penguin_train)
+
+# Feature engineering
+penguin_rec <- 
+  recipe(sex ~ ., data = penguin_train) %>% 
+  step_YeoJohnson(all_numeric_predictors()) %>%
+  step_dummy(species) %>% 
+  step_normalize(all_numeric_predictors())
+
+# Models ----
+# Log Regression
+glm_spec <- 
+  logistic_reg(penalty = 1) %>% 
+  set_engine("glm")
+
+# Random Forest
+tree_spec <- 
+  rand_forest(min_n = tune()) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
+
+# Torch model
+mlp_brulee_spec <- 
+  mlp(
+    hidden_units = tune(),
+    epochs = tune(),
+    penalty = tune(),
+    learn_rate = tune()
+  ) %>% 
+  set_engine("brulee") %>% 
+  set_mode("classification")
+
+# Bayes optimisation for Hyperparameter tuning----
+bayes_control <- control_bayes(no_improve = 10L,
+                               time_limit = 20,
+                               save_pred = TRUE,
+                               verbose = TRUE,
+                               seed = 5678)
+
+# Fit Models and Tune Hyper Parameters----
+workflow_set <- workflow_set(
+  preproc = list(penguin_rec),
+  models = list(glm = glm_spec,
+                tree = tree_spec,
+                torch = mlp_brulee_spec)
+) %>% 
+  workflow_map(
+    "tune_bayes",
+    iter = 50L,
+    resamples = penguin_folds,
+    control = bayes_control
+  )
+
+rank_results(workflow_set,
+             rank_metric = "roc_auc",
+             select_best = TRUE) %>% 
+  gt()
+
+workflow_set %>% 
+  autoplot()
+
+best_model_id <- "recipe_torch"
+
+# Select best model
+best_fit <- 
+  workflow_set %>% 
+  extract_workflow_set_result(best_model_id) %>% 
+  select_best(metric = "accuracy")
+
+final_workflow <- 
+  workflow_set %>% 
+  extract_workflow(best_model_id) %>% 
+  finalize_workflow(best_fit)
+
+final_fit <-
+  final_workflow %>% 
+  last_fit(penguin_split)
+
+#Fit model on the test/train data
+final_fit %>% 
+  collect_metrics() %>% 
+  gt()
+
+# Plot final metric
+final_fit %>% 
+  collect_predictions() %>% 
+  roc_curve(sex, .pred_female) %>% 
+  autoplot()
+
+# Create vetiver model
+
+
+
